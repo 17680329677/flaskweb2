fe15c90892eb053fbfae91fac20910f16fd371ab
17680329677 <38883463+17680329677@users.noreply.github.com>
2018-05-20 22:57:43 +0800

    chapter8(5-20)
    利用pychram完成了用户认证的所有模块

--- flaskweb2/app/models.py
+++ flaskweb2/app/models.py
@@ -4,6 +4,9 @@ from werkzeug.security import generate_password_hash, check_password_hash
 # flask_login专门用来管理用户认证系统中的认证状态 且不依赖特定的认证机制
 from flask_login import UserMixin
 from . import login_manager
+# itsdangerous提供了多种生成令牌的方法，其中TimedJSONWebSignatureSerializer类生成具有过期时间的JSON Web前面
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
 
 
 class Role(db.Model):
@@ -16,13 +19,14 @@ class Role(db.Model):
         return '<Role %r>' % self.name
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    confirmed = db.Column(db.Boolean, default=False)
 
     @property
     def password(self):
@@ -34,6 +38,26 @@ class User(db.Model):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    # 生成令牌 有效期为1小时
+    def generate_confirmation_token(self, expiration=3600):
+        # 这个类构造函数接受的参数是一个密钥，在Flask程序中可以使用SECRET_KEY设置
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        # dumps()函数为指定的数据生成一个加密签名，然后再对数据和签名进行序列化，生成令牌字符串，expiration设置过期时间
+        return s.dumps({'confirm': self.id}).decode('utf-8')
+
+    # 此方法验证令牌
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token.encode('utf-8'))
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
 
     def __repr__(self):
         return '<User %r>' % self.username
